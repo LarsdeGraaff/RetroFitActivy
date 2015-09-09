4417a575ab069032ad3e2eb546084e616e0da20c
LarsdeGraaff <lars.dg@gmail.com>
2015-09-09 12:24:07 +0200

    added print foto in imageView

--- app/src/main/java/com/example/jeansmits/activity/FotoActivity.java
+++ app/src/main/java/com/example/jeansmits/activity/FotoActivity.java
@@ -1,5 +1,6 @@
 package com.example.jeansmits.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Request;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -21,8 +26,9 @@ public class FotoActivity extends AppCompatActivity {
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int MEDIA_TYPE_IMAGE = 1;
     private File file;
+    public ImageView   imageViewPicture;
 
-    public void takePicture(View view){
+    public void takePicture(View view) {
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -35,6 +41,24 @@ public class FotoActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+           if (requestCode==CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE){
+
+               imageViewPicture= (ImageView) findViewById(R.id.imageViewPictureTaken);
+               Picasso.with(this).load("file://" + file.getAbsolutePath() ).into(imageViewPicture);
+
+           }
+
+        }
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +68,12 @@ public class FotoActivity extends AppCompatActivity {
     }
 
 
-
-
     private static File getOutputMediaFile(int type) {
         // To be safe, you should check that the SDCard is mounted
         // using Environment.getExternalStorageState() before doing this.
 
         File mediaStorageDir = new File(Environment.getExternalStorageDirectory(
-                ), "MyCameraApp");
+        ), "MyCameraApp");
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
 
@@ -65,9 +87,9 @@ public class FotoActivity extends AppCompatActivity {
 
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
+        if (type == MEDIA_TYPE_IMAGE) {
             mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
+                    "IMG_" + timeStamp + ".jpg");
 
         } else {
             return null;
